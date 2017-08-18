0782f2d35a03b74dabe2497ca62b8ecea5a40daf
davidmgudeman <davidmgudeman@gmail.com>
2017-08-18 07:45:51 -0700

    MainView: Added dropshadow to header

---
+++ TacoPop/DropShadow.swift
@@ -0,0 +1,21 @@
+//
+//  DropShadow.swift
+//  TacoPop
+//
+//  Created by David Gudeman on 8/18/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow {}
+
+extension DropShadow where Self: UIView {
+    func addDropShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 5
+    }
+    
+}
---
+++ TacoPop/HeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  HeaderView.swift
+//  TacoPop
+//
+//  Created by David Gudeman on 8/18/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView, DropShadow {
+  
+    
+}

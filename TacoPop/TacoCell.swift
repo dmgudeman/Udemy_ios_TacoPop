fc7d60623a3770682ce6b8b8e2b755fdea8212a2
davidmgudeman <davidmgudeman@gmail.com>
2017-08-18 08:39:16 -0700

    TacoCell was set up

---
+++ TacoPop/TacoCell.swift
@@ -0,0 +1,23 @@
+//
+//  TacoCell.swift
+//  TacoPop
+//
+//  Created by David Gudeman on 8/18/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+   
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    var taco: Taco!
+    
+    func configureCell(taco: Taco) {
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinId.rawValue)
+        tacoLabel.text = taco.productName
+    }
+
+}
